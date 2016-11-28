9397b830b7cda7ea43617ee9f43a51ecc7c88a18
Ernestas Mitkus <mitkus.ernestas@gmail.com>
2016-11-28 13:08:50 +0200

    move code from pirma.cpp to main.cpp and add a test file

--- pirma/main.cpp
+++ pirma/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <fstream>
+using namespace std;
+int main()
+{
+    int i, n, didziausias_sk, maziausias_sk;
+    ifstream fd("pirmas");
+    i=0;n=0;
+    int mas[100];
+    while(!fd.eof()){
+        fd >> mas[i];
+        i=i+1;
+    }
+    n=i;
+    fd.close();
+    for(i=0;i<=n-1;i++){
+        if(i==0){
+            didziausias_sk=mas[i];
+            maziausias_sk=mas[i];}
+        else if(mas[i]>didziausias_sk){
+            didziausias_sk=mas[i];}
+        if(mas[i]<maziausias_sk){
+            maziausias_sk=mas[i];}
+    }
+    ofstream fr("pirmas");
+    fr << "min " << maziausias_sk << endl;
+    fr << "max " << didziausias_sk << endl;
+    fr.close();
     return 0;
 }
